603f8c51280838841492907693015597fa2b0b8e
abhi-begowalia <abhisheakvermi75@gmail.com>
2024-02-15 17:02:31 +0530

    added new components

--- src/app/basic-concept/data-binding/data-binding.component.ts
+++ src/app/basic-concept/data-binding/data-binding.component.ts
@@ -17,4 +17,35 @@ export class DataBindingComponent {
   decreaseCounter(){
     this.counter = this.counter - 1;
   }
+
+  userData:any;
+  // userData:any = {
+  //   "name":"Abhisheak",
+  //   "age":23
+  // }
+
+  isSubmitting:boolean = true;
+  isActive:boolean = true;
+
+  pData = "Data for binding";
+  imgSrc= "../../assets/img.jpeg";
+
+  ToggleBtn(){
+    this.isSubmitting = !this.isSubmitting;
+  }
+
+  dStyle = {
+    color:'red',
+    bg:"blue",
+    font:20
+  }
+
+  multipleClasses = "colorRed font20";
+  eventclass = '';
+
+  toggleClass(){
+    this.eventclass == '' ? this.eventclass ="active" : this.eventclass = '';
+  }
+
+  isSelected:boolean = true;
 }
